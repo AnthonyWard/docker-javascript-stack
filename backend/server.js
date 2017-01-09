40ca953b5c1d29d2740ed3fe683421708a0e15d3
Developer <pants1984@gmail.com>
2017-01-09 16:12:46 +0000

    added a database

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,53 @@
 'use strict';
 
 const express = require('express');
+const bodyParser = require('body-parser');
 const cors = require('cors');
-
+const mongo = require('mongodb').MongoClient
+const databaseUrl = 'mongodb://mongo:27017/dockertest'
 // Constants
 const PORT = 8080;
+let db;
 
 // App
 const app = express();
 
 app.use(cors());
+app.use(bodyParser.json());
 
-app.get('/', function (req, res) {
-  res.json({ message: 'Hello from the backend 3' });
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello from node express' });
 });
 
-app.listen(PORT);
-console.log('Running on http://localhost:' + PORT);
+app.get('/notes', (req, res) => {
+  db.collection('notes').find().toArray((err, result) => {
+    if (err) return console.log(err)
+
+    res.json(result);
+  })
+})
+
+app.post('/notes', (req, res) => {
+  db.collection('notes').save(req.body, (err, result) => {
+    if (err) return console.log(err)
+
+    console.log('saved record to database')
+    res.json(result);
+  })
+});
+
+app.delete('/notes', (req, res) => {
+  db.collection('notes').deleteOne(req.body, (err, result) => {
+    if (err) return console.log(err)
+
+    res.json(result);
+  })
+})
+
+mongo.connect(databaseUrl, (err, database) => {
+  if (err) return console.log(err)
+  db = database
+  app.listen(PORT, () => {
+    console.log('Running on http://localhost:' + PORT);
+  })
+});
